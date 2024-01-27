f05a5f5545ab6ec4192f50d5e9e0edd0659b2aad
cwauge <32182433+cwauge@users.noreply.github.com>
2024-01-26 19:13:51 -1000

    Read file

--- PaperPlots.py
+++ PaperPlots.py
@@ -2,3 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from astropy.io import fits
 from astropy.io import ascii
+
+
+
+inf = ascii.read('/Users/connor_auge/Research/Disertation/GOALS/GOALS_cat/GOALS_full_table.csv')
